50f64c8f7566ec7870db1ddcd79564a027cc8bfb
Norbert Niziołek <norbert@niziolek.dev>
2024-09-14 00:23:55 +0200

    Set neutral color palette

--- src/const/theme.ts
+++ src/const/theme.ts
@@ -1,7 +1,7 @@
 import { extendTheme } from "@mui/joy";
 import { deepmerge } from "@mui/utils";
 
-// TODO add Inter font?
+// TODO add Inter/Karla font?
 
 const fontFamily = [
   "-apple-system",
@@ -17,6 +17,19 @@ const fontFamily = [
 ].join(",");
 
 const commonPalette = {
+  neutral: {
+    50: "#fafaf9",
+    100: "#f5f5f4",
+    200: "#e7e5e4",
+    300: "#d6d3d1",
+    400: "#a8a29e",
+    500: "#78716c",
+    600: "#57534e",
+    700: "#44403c",
+    800: "#292524",
+    900: "#1c1917",
+    950: "#0c0a09",
+  },
   primary: {
     50: "#fef2f2",
     100: "#fee2e2",
@@ -29,15 +42,16 @@ const commonPalette = {
     800: "#991b1b",
     900: "#7f1d1d",
     950: "#450a0a",
-
-    // TODO fix in light mode
-    softColor: "var(--joy-palette-primary-100)",
   },
 };
 
 const lightPalette = {};
 
-const darkPalette = {};
+const darkPalette = {
+  background: {
+    body: "var(--joy-palette-neutral-950)",
+  },
+};
 
 const fontSizeOriginal = {
   xs: 0.75,
@@ -59,6 +73,7 @@ const theme = extendTheme({
     display: fontFamily,
     body: fontFamily,
   },
+  // change default font size to 14px
   fontSize: Object.fromEntries(
     Object.entries(fontSizeOriginal).map(([key, value]) => [
       key,
