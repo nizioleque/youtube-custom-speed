c416445e0f11853f4379896552b576358d3adfcd
Norbert Niziołek <norbert@niziolek.dev>
2024-09-16 01:10:39 +0200

    Fix setting custom value

--- src/action/components/settings/RadioOption.tsx
+++ src/action/components/settings/RadioOption.tsx
@@ -1,4 +1,5 @@
 import { FormControl, FormHelperText, Input, Radio } from "@mui/joy";
+import { ChangeEvent, useEffect, useState } from "react";
 import type { RadioOptionData } from "../../../types";
 
 interface RadioOptionProps {
@@ -16,7 +17,39 @@ function RadioOption({
   onChange,
   onChangeCustom,
 }: RadioOptionProps) {
-  // TODO fix passing props (min max step type)
+  const [value, setValue] = useState<string>(customValue.toString());
+  const [error, setError] = useState<string | null>(null);
+
+  // make sure the text field doesn't show the default value (0)
+  useEffect(() => {
+    setValue(customValue.toString());
+  }, [customValue]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setError(null);
+
+    // update text field value
+    const value = event.currentTarget.value;
+
+    const isValid = /^[0-9]*[,.]?[0-9]*$/.test(value);
+    if (isValid) setValue(value);
+
+    // update storage value
+    const valueParsed = parseFloat(value.replace(",", "."));
+
+    if (isNaN(valueParsed)) {
+      setError("Invalid number");
+      return;
+    }
+
+    if (valueParsed < 0.0625 || valueParsed > 16) {
+      setError("Value is out of range (0.0625x - 16x)");
+      return;
+    }
+
+    // TODO support integers? (eg. add `transform` prop)
+    onChangeCustom(valueParsed);
+  };
 
   return (
     <>
@@ -31,23 +64,17 @@ function RadioOption({
         }}
       />
       {option.custom && (
-        <FormHelperText
-          sx={{
-            margin: "var(--FormHelperText-margin)",
-          }}
-        >
-          <FormControl>
+        <FormHelperText sx={{ margin: "var(--FormHelperText-margin)" }}>
+          <FormControl error={error !== null}>
             <Input
               size="sm"
-              value={customValue}
+              value={value}
+              placeholder="Custom value"
               onFocus={onChange}
-              onChange={(event) =>
-                // TODO support integers? (eg. add `transform` prop)
-                // TODO fix adding ./,
-                onChangeCustom(parseFloat(event.currentTarget.value))
-              }
-              // inputProps={option.customProps}
+              onChange={handleChange}
+              sx={{ width: 100 }}
             />
+            {error && <FormHelperText>{error}</FormHelperText>}
           </FormControl>
         </FormHelperText>
       )}
